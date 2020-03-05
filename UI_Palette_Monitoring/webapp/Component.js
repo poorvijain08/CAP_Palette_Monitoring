35bfbbc93217172a9350fc50f9a52bb2d5aee1e3
poorvi <poorvijain08@gmail.com>
2020-03-05 15:02:38 +0530

    commit message

---
+++ UI_Palette_Monitoring/webapp/Component.js
@@ -0,0 +1,7 @@
+sap.ui.define(["sap/suite/ui/generic/template/lib/AppComponent"], function (AppComponent) {
+	return AppComponent.extend(UI_Palette_Monitoring".Component", {
+        metadata: {
+	        manifest: "json"
+        },
+   });
+});
